a9e82e26306b40389b09283dee2968e5e82e4fbd
mstomar698 <tomarm698@gmail.com>
2023-03-31 20:02:46 +0530

    Adding medium quetion

---
+++ 81. Search in Rotated Sorted Array II.js
@@ -0,0 +1,18 @@
+// Replace X and Name_Here and fileName with Question Number and Question Statement
+/*
+* @lc app=leetcode id=81 lang=javascript
+*
+* [81]  Search in Rotated Sorted Array II
+*/
+ 
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    return nums.indexOf(target)>-1
+};
+ 
+// @lc code=end
