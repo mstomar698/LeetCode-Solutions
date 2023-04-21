0889e42b7fdff393683a778b8a0f0cd277a6014e
mstomar698 <tomarm698@gmail.com>
2023-04-21 20:28:52 +0530

    Adding hard-dp quetion

---
+++ 879. Profitable Schemes.js
@@ -0,0 +1,32 @@
+// Replace X and Name_Here and fileName with Question Number and Question Statement
+/*
+ * @lc app=leetcode id=879 lang=javascript
+ *
+ * [879]  Profitable Schemes
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} minProfit
+ * @param {number[]} group
+ * @param {number[]} profit
+ * @return {number}
+ */
+var profitableSchemes = function (n, minProfit, group, profit) {
+  let arr = new Array(n + 1).fill(0);
+  arr = arr.map(() => new Array(minProfit + 1).fill(0));
+  const mod = 10 ** 9 + 7;
+  arr.forEach((c) => (c[0] = 1));
+  group.forEach((item, key) => {
+    for (let i = n; i >= item; i--) {
+      for (let j = minProfit; j >= 0; j--) {
+        arr[i][j] =
+          (arr[i][j] + arr[i - item][Math.max(0, j - profit[key])]) % mod;
+      }
+    }
+  });
+  return arr[n][minProfit];
+};
+
+// @lc code=end
