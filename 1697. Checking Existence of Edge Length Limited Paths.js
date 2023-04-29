e88ccbc2c2a991838696034b7cb3fae9d2713e12
201b153 <tomarm698@gmail.com>
2023-04-29 12:03:28 +0530

    Adding hard-dp  question

---
+++ 1697. Checking Existence of Edge Length Limited Paths.js
@@ -0,0 +1,33 @@
+// Replace X and Name_Here and fileName with Question Number and Question Statement
+/*
+* @lc app=leetcode id=1697 lang=javascript
+*
+* [1697]  Checking Existence of Edge Length Limited Paths
+*/
+ 
+// @lc code=start
+/*
+ * @param {number} n
+ * @param {number[][]} edgeList
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
+var distanceLimitedPathsExist = function(n, edgeList, queries) {
+    let index = new Array(n).fill().map((_,i) => i);
+    let keys = new Array(queries.length).fill().map((_,i) => i)
+    let result = new Array(queries.length), j = 0
+
+    const find = (x) => x !== index[x] ? index[x] = find(index[x]) : index[x]
+    const union = (x,y) => index[find(x)] = find(y)
+    edgeList.sort((a,b) => a[2] - b[2])
+    keys.sort((a,b) => queries[a][2] - queries[b][2])
+
+    for (let i of keys) {
+        let [a,b,c] = queries[i]
+        while (edgeList[j]?.[2] < c) union(edgeList[j][0], edgeList[j++][1])
+        result[i] = find(a) === find(b)
+    }
+    return result
+};
+ 
+// @lc code=end
