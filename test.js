3fb295aead274909c2543e37e6ccdf4d127b7329
201b153 <tomarm698@gmail.com>
2023-05-13 09:20:15 +0530

    Adding question tesing

---
+++ test.js
@@ -0,0 +1,37 @@
+(function test() {
+  console.log('Testing CP questions');
+})();
+
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+  const memo = {};
+  return function (...args) {
+    const params = JSON.stringify(args);
+    if (params in memo) return memo[params];
+    const result = fn(...args);
+    memo[params] = result;
+    return result;
+  };
+}
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
