8d1ec0db962fc93a4a0b585a154fbd406c06701e
201b153 <tomarm698@gmail.com>
2023-05-14 07:13:21 +0530

    Adding hard question

---
+++ 1799. Maximize Score After N Operations.js
@@ -0,0 +1,48 @@
+// Replace X and Name_Here and fileName with Question Number and Question Statement
+/*
+* @lc app=leetcode id=1799 lang=javascript
+*
+* [1799]  Maximize Score After N Operations
+*/
+ 
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxScore = function(nums) {
+    
+    function gcd(a, b) {
+        if(!b) return a;
+        return gcd(b, a % b);
+    }
+    
+    const memo = new Map();
+    
+    function recurse(arr, num1, op) {
+        if(!arr.length) return 0;
+        
+        const key = arr.join() + num1;
+        if(memo.has(key)) return memo.get(key);
+        
+        let max = 0;
+        
+        for(let i = 0; i < arr.length; i++) {
+            const nextArr = [...arr.slice(0, i), ...arr.slice(i+1)];
+            
+            if(num1) {
+                const currGCD = gcd(num1, arr[i]);
+                const rest = recurse(nextArr, null, op+1);
+                max = Math.max(max, ((op * currGCD) + rest));
+            } else {
+                const rest = recurse(nextArr, arr[i], op);
+                max = Math.max(max, rest);
+            }
+        }
+        memo.set(key, max);
+        return max;
+    }
+    return recurse(nums, null, 1);
+};
+ 
+// @lc code=end
