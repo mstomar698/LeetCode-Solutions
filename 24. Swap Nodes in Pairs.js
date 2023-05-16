18e6ef7e513e62d746013121e1886584b9f5cae2
mstomar698 <tomarm698@gmail.com>
2023-05-16 19:29:10 +0530

    Adding medium question

--- 24. Swap Nodes in Pairs.js
+++ 24. Swap Nodes in Pairs.js
@@ -6,18 +6,30 @@
  */
 
 // @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var swapPairs = function (head) {
-  if (head == null || head.next == null) return head;
-
-  let currHead = head;
-  let nextHead = head.next;
-  let skipHead = head.next.next;
-
-  nextHead.next = currHead;
-
-  currHead.next = swapPairs(skipHead);
-
-  return nextHead;
-};
+    if (head == null || head.next == null) return head;
+  
+    let currHead = head;
+    let nextHead = head.next;
+    let skipHead = head.next.next;
+  
+    nextHead.next = currHead;
+  
+    currHead.next = swapPairs(skipHead);
+  
+    return nextHead;
+  };
+  
 
 // @lc code=end
